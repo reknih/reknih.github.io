429485e754020c6b3575c610bdfa2cdf87c0e577
Martin Haug <mhaug@live.de>
2018-04-09 08:30:57 +0200

    New Musica Reifenberg design in portfolio

--- portfolio.md
+++ portfolio.md
@@ -5,6 +5,9 @@ layout: portfolio
 - ![udojay.de Homepage Desktop-Ansicht]({{ site.github.url }}/images/udojay.png)
 ## [Udo Jay](https://udojay.de/)
 Diese maßgeschneiderte Homepage für Andreas Schwarze, besser bekannt als das Udo Jürgens-Double Udo Jay, zeigt ihren Besuchern Impressionen von den vergangenen Auftritten des Künstlers. Wer von dem, was er sieht, überzeugt ist, kann sich schon vor dem Senden einer Anfrage je nach Art des Konzerts einen Preisbereich berechnen lassen.
+- ![musica-reifenberg.de Homepage]({{ site.github.url }}/images/musicaReifenberg.png)
+## [Musica Reifenberg](https://www.musica-reifenberg.de/)
+Auf Musica Reifenbergs Website gibt es Hörproben und Besucher werden im Blog über die nächsten Auftritte des Ensembles informiert. Die Seite sieht natürlich sowohl auf dem Desktop als auch auf dem Handy gut aus!
 - ![centrallink.de Startseite]({{ site.github.url }}/images/centrallink.png)
 ## [central_link](http://centrallink.de/)
 Dieses Portal wirbt für die von mir mit einem Freund in Vergangenheit entwickelten mobilen Apps und ist auf Desktops und Smartphones gleichermaßen optimiert. Mit einem Blog und zwei internationalisierten Versionen richtet es sich an ein großes Publikum.
@@ -20,9 +23,6 @@ Als Musiker im Rhein-Main-Gebiet hat Christian Schröder viel zu tun und ein umf
 - ![Dr. Barbara Günther-Haug's Psychoticker]({{ site.github.url }}/images/psychoticker.png)
 ## [Dr. med. Barbara Günther-Haug](http://www.blog.b-guenther-haug-psychotherapie.de/wordpress/)
 Dr. Günther-Haug ist eine Ärztin für Psychotherapie in Schmitten. Ihre Website ist nicht nur das digitale Aushängeschild ihrer Praxis, sondern beinhaltet auch einen großen Blog, in dem immer wieder Themen über der Psychologie schreibt.
-- ![musica-reifenberg.de Homepage]({{ site.github.url }}/images/musicaReifenberg.png)
-## [Musica Reifenberg](https://musica-reifenberg.de/)
-Musica Reifenbergs Website gibt es Hörproben und Videos, Besucher werden im Blog über die nächsten Auftritte des Ensembles informiert oder können gleich buchen.  
 - ![CWS Informant auf verschiedenen Geräten]({{ site.github.url }}/images/informant.png)
 ## [CWS Informant](http://centrallink.de/informant/)
 Der CWS Informant ist eine App für das Vertretungsplansystem der Christian-Wirth-Schule, die ich als [Open-Source-Projekt](https://github.com/reknih/UntisExp/) entwickelt habe. Es gibt sie für [Android](https://play.google.com/store/apps/details?id=vplan.vplan) und [Windows Phone](https://www.microsoft.com/de-de/store/p/cws-informant/9wzdncrfjw4k?wa=wsignin1.0&rtc=1), früher war auch eine iOS-Version verfügbar. Das Projekt baut auf Xamarin auf, um eine große Cross-Platform-Kompatibilität zu erhalten und bietet den Schülern eine viel bessere Benutzererfahrung als das Webangebot der Schule
--- scripts/latexworker.js
+++ scripts/latexworker.js
@@ -1,125 +1,127 @@
 function prog2presentation(str) {
 	return str.replace(/(XOR)|(\^)/ig, "⊕")
-		.replace(/(\|\|)|(OR)/g, "∨")
-		.replace(/!|(NOT)/ig, "¬")
-		.replace(/(element)|(in)/ig, "∈")
-		.replace(/(False)|\sF\s|(Bottom)/ig, "⊥")
-		.replace(/(True)|\sT\s|(Top)/ig, "T")
-		.replace(/(&&)|(AND)/ig, "∧")
-		.replace(/(<->)|(<=>)/ig, "↔")
-		.replace(/(->)|(implies)|(=>)/gi, "→")
-		.replace(/alpha/ig, "α")
-		.replace(/beta/ig, "β")
-		.replace(/gamma/g, "γ")
-		.replace(/Gamma/ig, "Γ")
-		.replace(/delta/g, "δ")
-		.replace(/Delta/ig, "∆")
-		.replace(/eps(ilon)?/ig, "ε")
-		.replace(/zeta/ig, "ζ")
-		.replace(/theta/g, "θ")
-		.replace(/Theta/ig, "Θ")
-		.replace(/lambda/ig, "λ")
-		.replace(/(mu)|(micro)/ig, "μ")
-		.replace(/xi/g, "ξ")
-		.replace(/Xi/ig, "Ξ")
-		.replace(/pi/g, "π")
-		.replace(/Pi/ig, "Π")
-		.replace(/sigma/g, "σ")
-		.replace(/Sigma/ig, "Σ")
-		.replace(/tau/ig, "τ")
-		.replace(/phi/ig, "φ")
-		.replace(/chi/ig, "χ")
-		.replace(/\spsi/ig, "ψ")
-		.replace(/omega/g, "ω")
-		.replace(/Omega/ig, "Ω")
-		.replace(/===/g, "≡")
-		.replace(/\*/g, "×")
-		.replace(/\//g, "÷")
-		.replace(/>\s?=/g, "≥")
-		.replace(/(<\s?=)|(=\s?<)/g, "≤")
-		.replace(/sqrt\((.*?)\)/g, "√($1)")
-		.replace(/floor\((.*?)\)/g, "└$1┘")
-		.replace(/ceil\((.*?)\)/g, "┌$1┐")
-		.replace(/{}/g, "∅")
-		.replace(/infinity/g, "∞")
+	.replace(/(\|\|)|(OR)/g, "∨")
+	.replace(/!|(NOT)/ig, "¬")
+	.replace(/(element)|(in)/ig, "∈")
+	.replace(/(False)|\sF\s|(Bottom)/ig, "⊥")
+	.replace(/(True)|\sT\s|(Top)/ig, "T")
+	.replace(/(&&)|(AND)/ig, "∧")
+	.replace(/(<->)|(<=>)/ig, "↔")
+	.replace(/(->)|(implies)|(=>)/gi, "→")
+	.replace(/alpha/ig, "α")
+	.replace(/beta/ig, "β")
+	.replace(/gamma/g, "γ")
+	.replace(/Gamma/ig, "Γ")
+	.replace(/delta/g, "δ")
+	.replace(/Delta/ig, "∆")
+	.replace(/eps(ilon)?/ig, "ε")
+	.replace(/zeta/ig, "ζ")
+	.replace(/theta/g, "θ")
+	.replace(/Theta/ig, "Θ")
+	.replace(/lambda/ig, "λ")
+	.replace(/(mu)|(micro)/ig, "μ")
+	.replace(/xi/g, "ξ")
+	.replace(/Xi/ig, "Ξ")
+	.replace(/pi/g, "π")
+	.replace(/Pi/ig, "Π")
+	.replace(/sigma/g, "σ")
+	.replace(/Sigma/ig, "Σ")
+	.replace(/tau/ig, "τ")
+	.replace(/phi/ig, "φ")
+	.replace(/chi/ig, "χ")
+	.replace(/\spsi/ig, "ψ")
+	.replace(/omega/g, "ω")
+	.replace(/Omega/ig, "Ω")
+	.replace(/===/g, "≡")
+	.replace(/\*/g, "×")
+	.replace(/\//g, "÷")
+	.replace(/>\s?=/g, "≥")
+	.replace(/(<\s?=)|(=\s?<)/g, "≤")
+	.replace(/sqrt\((.*?)\)/g, "√($1)")
+	.replace(/floor\((.*?)\)/g, "└$1┘")
+	.replace(/ceil\((.*?)\)/g, "┌$1┐")
+	.replace(/{}/g, "∅")
+	.replace(/infinity/g, "∞")
 }
 
 function prog2latex(str) {
 	return str.replace(/(XOR)|(\^)/ig, "\\oplus")
-		.replace(/(\|\|)|(OR)/g, "\\vee")
-		.replace(/!\s?=/g, "\\neq")
-		.replace(/\|=/g, "\\vDash")
-		.replace(/!|(NOT)/ig, "\\neg ")
-		.replace(/infinity/ig, "∞")
-		.replace(/(element)|(in)/ig, "\\in")
-		.replace(/(!\s?element)|(!\s?in)/ig, "\\notin")
-		.replace(/{}/g, "\\emptyset")
-		.replace(/(False)|\sF\s|(Bottom)/ig, "\\perp{}")
-		.replace(/(True)|\sT\s|(Top)/ig, "\\top{}")
-		.replace(/(&&)|(AND)/ig, "\\wedge")
-		.replace(/(<->)|(<=>)/ig, "\\leftrightarrow")
-		.replace(/(->)|(implies)|(=>)/ig, "\\rightarrow")
-		.replace(/alpha/ig, "\\alpha")
-		.replace(/alpha/ig, "\\alpha")
-		.replace(/\n/ig, "\\\\\n")
-		.replace(/beta/ig, "\\beta")
-		.replace(/(g)amma/ig, "\\$1amma")
-		.replace(/(d)elta/ig, "\\$1elta")
-		.replace(/eps(ilon)?/ig, "\\varepsilon")
-		.replace(/zeta/ig, "\\zeta")
-		.replace(/(t)heta/ig, "\\$1heta")
-		.replace(/lambda/ig, "\\lambda")
-		.replace(/(mu)|(micro)/ig, "\\mu")
-		.replace(/(x)i/ig, "\\$1i")
-		.replace(/(p)i/ig, "\\$1i")
-		.replace(/(S)igma/ig, "\\$1igma")
-		.replace(/tau/ig, "\\tau")
-		.replace(/phi/ig, "\\varphi")
-		.replace(/chi/ig, "\\chi")
-		.replace(/\spsi/ig, "\\psi")
-		.replace(/(O)mega/ig, "\\$1mega")
-		.replace(/===/g, "\\equiv")
-		.replace(/:=/g, "\\coloneqq")
-		.replace(/\.\.\./g, "\\cdots")
-		.replace(/\*/g, "\\times")
-		.replace(/\//g, "\\div")
-		.replace(/>\s?=/g, "\\geq")
-		.replace(/(<\s?=)|(=\s?<)/g, "\\leq")
-		.replace(/\[\[/g, "\\llbracket")
-		.replace(/\]\]/g, "\\rrbracket")
-		.replace(/%/g, "\\%")
-		.replace(/#/g, "\\#")
-		.replace(/:/g, "\\:")
-		.replace(/\$/g, "\\$")
-		.replace(/sqrt\((.*?)\)/ig, "\\sqrt{$1}")
-		.replace(/floor\((.*?)\)/ig, "\\lfloor $1\\rfloor")
-		.replace(/ceil\((.*?)\)/ig, "\\lceil $1\\rceil")
-		.replace(/∞/g, "\\infty")
+	.replace(/(\|\|)|(OR)/g, "\\vee")
+	.replace(/!\s?=/g, "\\neq")
+	.replace(/\|=/g, "\\vDash")
+	.replace(/!|(NOT)/ig, "\\neg ")
+	.replace(/infinity/ig, "∞")
+	.replace(/(element)|(in)/ig, "\\in")
+	.replace(/(!\s?element)|(!\s?in)/ig, "\\notin")
+	.replace(/{}/g, "\\emptyset")
+	.replace(/(False)|\sF\s|(Bottom)/ig, "\\perp{}")
+	.replace(/(True)|\sT\s|(Top)/ig, "\\top{}")
+	.replace(/(&&)|(AND)/ig, "\\wedge")
+	.replace(/(<->)|(<=>)/ig, "\\leftrightarrow")
+	.replace(/(->)|(implies)|(=>)/ig, "\\rightarrow")
+	.replace(/alpha/ig, "\\alpha")
+	.replace(/alpha/ig, "\\alpha")
+	.replace(/\n/ig, " \\\\\n")
+	.replace(/beta/ig, "\\beta")
+	.replace(/(g)amma/ig, "\\$1amma")
+	.replace(/(d)elta/ig, "\\$1elta")
+	.replace(/eps(ilon)?/ig, "\\varepsilon")
+	.replace(/zeta/ig, "\\zeta")
+	.replace(/(t)heta/ig, "\\$1heta")
+	.replace(/lambda/ig, "\\lambda")
+	.replace(/(mu)|(micro)/ig, "\\mu")
+	.replace(/(x)i/ig, "\\$1i")
+	.replace(/(p)i/ig, "\\$1i")
+	.replace(/(S)igma/ig, "\\$1igma")
+	.replace(/tau/ig, "\\tau")
+	.replace(/phi/ig, "\\varphi")
+	.replace(/chi/ig, "\\chi")
+	.replace(/\spsi/ig, "\\psi")
+	.replace(/(O)mega/ig, "\\$1mega")
+	.replace(/===/g, "\\equiv")
+	.replace(/:=/g, "\\coloneqq")
+	.replace(/\.\.\./g, "\\cdots")
+	.replace(/\*/g, "\\times")
+	.replace(/\//g, "\\div")
+	.replace(/>\s?=/g, "\\geq")
+	.replace(/(<\s?=)|(=\s?<)/g, "\\leq")
+	.replace(/\[\[/g, "\\llbracket")
+	.replace(/\]\]/g, "\\rrbracket")
+	.replace(/%/g, "\\%")
+	.replace(/#/g, "\\#")
+	.replace(/:/g, "\\:")
+	.replace(/\$/g, "\\$")
+	.replace(/sqrt\((.*?)\)/ig, "\\sqrt{$1}")
+	.replace(/floor\((.*?)\)/ig, "\\lfloor $1\\rfloor")
+	.replace(/ceil\((.*?)\)/ig, "\\lceil $1\\rceil")
+	.replace(/∞/g, "\\infty")
 }
+
+var 
 
 // courtesy of https://codereview.stackexchange.com/questions/45991/balanced-parentheses
 function parenthesesAreBalanced(string) {
-  var parentheses = "[]{}()",
-    stack = [],
-    i, character, bracePosition;
+	var parentheses = "[]{}()",
+	stack = [],
+	i, character, bracePosition;
 
-  for(i = 0; character = string[i]; i++) {
-    bracePosition = parentheses.indexOf(character);
+	for(i = 0; character = string[i]; i++) {
+		bracePosition = parentheses.indexOf(character);
 
-    if(bracePosition === -1) {
-      continue;
-    }
-
-    if(bracePosition % 2 === 0) {
-     	stack.push(bracePosition + 1); // push next expected brace position
-    } else {
-    	if(stack.pop() !== bracePosition) {
-			return false;
+		if(bracePosition === -1) {
+			continue;
 		}
-    }
-  }
 
-  return stack.length === 0;
+		if(bracePosition % 2 === 0) {
+     	stack.push(bracePosition + 1); // push next expected brace position
+     } else {
+     	if(stack.pop() !== bracePosition) {
+     		return false;
+     	}
+     }
+ }
+
+ return stack.length === 0;
 }
 
 function matchInArray(str, a) {
